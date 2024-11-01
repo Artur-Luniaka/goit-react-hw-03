0337106b9a76de06880395761c3a87dba680f170
Artur Luniaka <artur01049718@gmail.com>
2024-11-01 08:52:50 +0100

    finished second step/added light styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import "modern-normalize/modern-normalize.css";
 import { useState } from "react";
 import contacts from "./assets/contacts.json";
 import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 const App = () => {
   const [contact, setContact] = useState(contacts);
+  const [searchContact, setSearchContact] = useState("");
+  const handleSearchContact = (e) => {
+    setSearchContact(e.target.value);
+  };
+  const handleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchContact.toLowerCase())
+  );
+
   return (
     <>
       <h1>Phonebook</h1>
-      <ContactList contacts={contacts} />
+      <SearchBox onChange={handleSearchContact} />
+      <ContactList contacts={handleContacts} />
     </>
   );
 };
